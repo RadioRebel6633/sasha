90626733a4395a2aa64108b1abb1f013ddb66ee8
Cody Braun <42684239+radiogrammer@users.noreply.github.com>
2018-12-13 21:59:25 -0500

    added open gmail functionality and modules folder

--- sasha.py
+++ sasha.py
@@ -1,36 +1,93 @@
-from gtts import gTTS
 import speech_recognition as sr
 import os
 import time 
 import webbrowser
+import winspeech 
+import pygame
+from pygame import mixer
+import sys
+from modules import checkEmail
+sys.path.append("C:/Users/cody/Desktop/test/modules")
+from checkEmail import openEmail
 def talk(audio):
-	tts=gTTS(text=audio,lang ="en" )
-	tts.save("file1.mp3")
-	os.system("start file1.mp3")
+	winspeech.say(audio)
+	
 def listenForCmd():
 
-	
 	r = sr.Recognizer()
 	
 	with sr.Microphone() as source:
 		print("Hello. i am SASHA. i'm online and ready")
+		r.pause_threshold = 1
 		r.adjust_for_ambient_noise(source, duration = 1)
 		audio = r.listen(source)
+		
 	try:
-		command = r.recognize_google(audio)
-		print(command)
+		command = r.recognize_google(audio).lower()
+		print("Your command:", command)
 	
 	except sr.UnknownValueError:
 		print("the mic isnt working right... i didnt hear any command")
-		command =listenForCmd()
+		command = listenForCmd()
 		
 	return command
-command = listenForCmd()
-def cmd():
-	if "hey" in command:
-		os.system('start sounds/Bidding.mp3')
+#The pitch was controlled by the freqency argument in mixer.init(). So I just set that higher
+
+def cmd(command): #function for playing user's commands
+	
+	if "what up" in command:
+		mixer.init()
+		mixer.music.load("C:/Users/cody/Desktop/test/sounds/Bidding.mp3")
+		mixer.music.play()
+		while mixer.music.get_busy():
+			pygame.time.Clock().tick(10)
+
+
+	elif "defeated by victory" in command:
+		mixer.init(frequency = 30000)
+		mixer.music.load("C:/Users/cody/Desktop/test/sounds/defeated_by_victory.ogg")
+		mixer.music.play()
+		while mixer.music.get_busy():
+			pygame.time.Clock().tick(10)
+
+		
+	elif "shakespeare" in command:
+		mixer.init(frequency = 30000)
+		mixer.music.load("C:/Users/cody/Desktop/test/sounds/shakespeare (1).ogg")
+		mixer.music.play()
+		while mixer.music.get_busy():
+			pygame.time.Clock().tick(13)
+		
+		
+	elif "you're weird" in command:
+		mixer.init()
+		mixer.music.load("C:/Users/cody/Desktop/test/eatmyshorts.wav")
+		mixer.music.play()
+		while mixer.music.get_busy():
+			pygame.time.Clock().tick(1)
+
+	elif 'listen here' in command: 
+		mixer.init(frequency = 30000)
+		mixer.music.load("C:/Users/cody/Desktop/test/sounds/listening.mp3")
+		mixer.music.play()
+		while mixer.music.get_busy():
+			pygame.time.Clock().tick(1)
+		
+	elif 'check my email' in command:
+		talk("opening email")
+		checkEmail.openEmail()
+		
+	elif 'victory' in command: 
+		mixer.init(frequency = 30000)
+		mixer.music.load("C:/Users/cody/Desktop/test/sounds/yay.mp3")
+		mixer.music.play()
+		while mixer.music.get_busy():
+			pygame.time.wait(5)
+
+	mixer.quit()
+
+
 talk("ready when you are")
+
 while True:
-	listenForCmd()
-	cmd()
-	time.sleep(3)
+	cmd(listenForCmd())
